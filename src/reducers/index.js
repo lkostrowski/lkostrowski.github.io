f352d03c7cb3629128ba00ce1b53b83c0c8cda67
Łukasz Ostrowski <lukasz.ostrowski@creativestyle.pl>
2017-07-18 10:23:04 +0200

    Updates

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux'
-import filters from './filters';
-import shelf from './pages/shelf';
-
-export default combineReducers({
-    filters,
-    pagesShelf: shelf
-});
